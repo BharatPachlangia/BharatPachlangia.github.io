958077c64ca78abb3554a7a864eac871cd6834dc
BharatPachlangia <119922781+BharatPachlangia@users.noreply.github.com>
2022-12-08 16:21:18 +0530

    Create goodbye.js

---
+++ Module 4/js/goodbye.js
@@ -0,0 +1,9 @@
+ byeSpeaker = {};
+    var speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
