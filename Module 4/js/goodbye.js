a63e0b42e51f6564ba6d16e0f9e0b67c98f051cc
BharatPachlangia <119922781+BharatPachlangia@users.noreply.github.com>
2022-12-08 16:24:10 +0530

    Delete Module 4/js directory

--- Module 4/js/goodbye.js
+++
@@ -1,9 +0,0 @@
- byeSpeaker = {};
-    var speakWord = "Good Bye";
-    
-    byeSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-     
-     window.byeSpeaker = byeSpeaker;
-    })(window);
